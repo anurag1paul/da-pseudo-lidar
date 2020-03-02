0fa55fcb3a5460cee7f5a4c17cb0915398228792
Anurag Paul <anurag1paul@gmail.com>
2020-03-02 14:55:19 -0800

    fix config issues

--- frustum_pointnet/configs/vkitti/__init__.py
+++ frustum_pointnet/configs/vkitti/__init__.py
@@ -7,5 +7,5 @@ configs.data.num_classes = len(configs.data.classes)
 # evaluate configs
 configs.evaluate = Config()
 configs.evaluate.num_tests = 20
-configs.evaluate.ground_truth_path = 'data/vkitti/raw_data/'
-configs.evaluate.scene = 'Scene20'
+configs.evaluate.ground_truth_path = 'data/kitti/ground_truth'
+configs.evaluate.image_id_file_path = 'data/kitti/image_sets/val.txt'
