3545dd337eae1b9dbb17eae11d50cfafd89f8e1c
Anurag Paul <anurag1paul@gmail.com>
2020-03-03 13:17:15 -0800

    fixing reject points

--- preprocessing/vkitti/vkitti_object.py
+++ preprocessing/vkitti/vkitti_object.py
@@ -99,8 +99,7 @@ class vkitti_object(object):
         depth = self.get_depth_map(idx)
         velo = project_depth_to_points(calib, depth)
         velo = np.concatenate([velo, np.ones((velo.shape[0], 1))], 1)
-        points = 0.5   # percentage of points to be rejected
-        points = 0.5   # percentage of points to be rejected
+        points = 0.25   # percentage of points to be rejected
         points_step = int(1. / points)
         velo_range = range(0, velo.shape[0], points_step)
         velo_frame = velo[velo_range, :]
