2fcd375b36b17dc54685f1d6aabfe16b2308828d
Anurag Paul <anurag1paul@gmail.com>
2020-03-19 21:10:04 -0700

    reducing num_tests to 3

--- frustum_pointnet/configs/kitti/__init__.py
+++ frustum_pointnet/configs/kitti/__init__.py
@@ -6,6 +6,6 @@ configs.data.num_classes = len(configs.data.classes)
 
 # evaluate configs
 configs.evaluate = Config()
-configs.evaluate.num_tests = 20
+configs.evaluate.num_tests = 3
 configs.evaluate.ground_truth_path = 'data/kitti/ground_truth'
 configs.evaluate.image_id_file_path = 'data/kitti/image_sets/val.txt'
